a775f906d74e2c67051b60f44a788d636498495c
Mokowz <ronniemokaya30@gmail.com>
2023-09-13 00:36:25 +0300

    Login user

--- records/views.py
+++ records/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        # Authenticate user
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login Successful")
+            return redirect('home')
+        else:
+            messages.success(request, "There was an eror logging in")
+            return redirect('home')
+    
+    else:
+        return render(request, 'home.html', )
+
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
