54392475eb38d49768e7034534770aff6ddd80ba
Mokowz <ronniemokaya30@gmail.com>
2023-09-13 11:33:46 +0300

    View records

--- records/views.py
+++ records/views.py
@@ -3,10 +3,11 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 def home(request):
-
+    records = Record.objects.all()
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
@@ -23,7 +24,8 @@ def home(request):
             return redirect('home')
     
     else:
-        return render(request, 'home.html', )
+        return render(request, 'home.html', {"records": records})
+    
 
 
 
