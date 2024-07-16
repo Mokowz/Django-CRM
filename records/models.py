75207127e16c25727c42078755fad22ed31f0991
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-07-16 20:00:41 +0300

    Remove linter-error line

--- records/models.py
+++ records/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Record(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-email = models.EmailField(max_length=100)
+    email = models.EmailField(max_length=100)
     phone = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
