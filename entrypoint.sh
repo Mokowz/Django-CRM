4b4210b0e4d9bc3d9c6f42c349e72a128e7f7408
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-07-16 12:37:24 +0300

    Add an example workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
 
     while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
+      sleep 0.6
     done
 
     echo "PostgreSQL started"
